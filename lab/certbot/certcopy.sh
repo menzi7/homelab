1ce01c201d62cc6e09c268dfde20a20a446dba45
menzi7 <tobias.bondo@gmail.com>
2025-09-13 23:30:41 +0200

    Moved containers to /opt/ & added dockge and actual

--- lab/certbot/certcopy.sh
+++ lab/certbot/certcopy.sh
@@ -2,12 +2,19 @@
 
 # Directory variables
 SOURCE_DIR="/etc/letsencrypt/archive/pi.menzi.dk"
-UNIFI_DIR="/home/tme/containers/UnifiController/cert"
-ZABBIX_DIR="/home/tme/containers/Zabbix/zbx_env/etc/ssl/nginx"
-HA_DIR="/home/tme/containers/HomeAssistant/cert"
-VAULTWARDEN_DIR="/home/tme/containers/Vaultwarden/cert"
+DOCKGE_DIR="/opt/dockge/cert"
+UNIFI_DIR="/opt/containers/unifiController/cert"
+ZABBIX_DIR="/opt/containers/zabbix/zbx_env/etc/ssl/nginx"
+HA_DIR="/opt/containers/homeAssistant/cert"
+VAULTWARDEN_DIR="/opt/containers/vaultwarden/cert"
+ACTUAL_DIR="/opt/containers/actual/data"
 
 # copy newest cert to container folder
+
+# Dockge
+cp -f $(ls -t ${SOURCE_DIR}/fullchain*.pem | head -n 1) ${DOCKGE_DIR}/fullchain.pem
+cp -f $(ls -t ${SOURCE_DIR}/privkey*.pem | head -n 1) ${DOCKGE_DIR}/privkey.pem
+docker restart dockge
 
 # Unifi
 cp -f $(ls -t ${SOURCE_DIR}/fullchain*.pem | head -n 1) ${UNIFI_DIR}/fullchain.pem
@@ -30,3 +37,8 @@ docker restart homeassistant
 cp -f $(ls -t ${SOURCE_DIR}/fullchain*.pem | head -n 1) ${VAULTWARDEN_DIR}/fullchain.pem
 cp -f $(ls -t ${SOURCE_DIR}/privkey*.pem | head -n 1) ${VAULTWARDEN_DIR}/privkey.pem
 docker restart vaultwarden
+
+# Actual
+cp -f $(ls -t ${SOURCE_DIR}/fullchain*.pem | head -n 1) ${ACTUAL_DIR}/ssl.crt
+cp -f $(ls -t ${SOURCE_DIR}/privkey*.pem | head -n 1) ${ACTUAL_DIR}/ssl.key
+docker restart actual
